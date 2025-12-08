f8984814345b702b4dae7fbbf5b6f18370afc313
AngelinaVB <b.lina.1995@mail.ru>
2025-12-08 20:42:34 +0300

    задание готово

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,47 @@
-export default class Character {
-  constructor(elem) {
-    this.cell = elem.querySelectorAll(".grid-cell");
-    this.icon = elem.querySelector(".icon");
-  }
+import goblin from '../img/goblin.png'
 
-  getRandomPosition() {
-    let previous = 0;
-    let i = 0;
+function createBoard() {
+  const board = document.getElementById("board");
 
-    setInterval(() => {
-      while (i === previous) {
-        i = Math.floor(Math.random() * (this.cell.length - 1));
-      }
-
-      previous = i;
-      this.cell[i].appendChild(this.icon);
-    }, 1000);
+  for (let i = 0; i < 16; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    board.appendChild(cell);
   }
 }
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function createCharacter() {
+  const img = document.createElement('img');
+  img.src = 'src/img/goblin.png';
+  img.alt = 'Изображение персонажа';
+
+  const randomCell = document.querySelector(
+    `.cell:nth-child(${getRandomInt(1, 17)})`,
+  );
+  randomCell.appendChild(img);
+
+  return img;
+}
+
+export default function moveCharacter(character) {
+  const allCells = document.querySelectorAll(".cell");
+  let currentCell = character.parentNode;
+
+  let newCell;
+  do {
+    newCell = allCells[getRandomInt(0, 16)];
+  } while (newCell === currentCell);
+
+  newCell.appendChild(character);
+}
+
+createBoard();
+const character = createCharacter();
+
+setInterval(() => {
+  moveCharacter(character);
+}, 1000);
